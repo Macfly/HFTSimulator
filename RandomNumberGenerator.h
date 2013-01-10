0815be13269675e410605826b1a07d7d7efec535
CorentinValleroy <cocoval@gmail.com>
2013-01-10 15:05:59 +0100

    add seed to RNG

--- RandomNumberGenerator.h
+++ RandomNumberGenerator.h
@@ -16,6 +16,7 @@ class RandomNumberGenerator
 		RandomNumberGenerator()
 		{
 			m_rng=GeneratorType();
+			m_rng.seed(static_cast<unsigned int>(std::time(0)));
 		};
 		~RandomNumberGenerator(){};
 		GeneratorType& getRNG()
